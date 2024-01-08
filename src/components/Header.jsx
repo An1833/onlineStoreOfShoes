118c7bd84f2f70a41b9629e2301c041b9b04f411
An1833 <6225562@gmail.com>
2024-01-08 16:37:22 +0300

    add favorite 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,20 @@ function Header (props) {
         <img  width={25} height={15} src="/img/logo1.png" alt="shoes"/>
         <div>
           <h3 className="text-uppercase">Online story</h3>
-          <p className="opacity -5">Лучший магазин Андрея</p>
+          <p className="opacity -5">Лучший магазин обуви</p>
         </div>
         </div>
         <ul className="d-flex justify-between" >
           <li onClick = {props.onClickCart} className="mr-30 cu-p">
-            <img className="svg" src="/img/icon/cart.svg" />
+            <img className="svg" src="/img/icon/cart.svg" alt="Cart" />
             <span>1205 руб.</span>
           </li>
+          <li className="heart_svg mr-20 cu-p">
+            <img className="svg" src="/img/icon/heart.svg" alt="Bookmarks"/>
+          </li>
           <li>
-          <img src="/img/icon/user.svg" />
-          <span>MAN</span>
+          <img src="/img/icon/user.svg" alt="User" />
+          <span>User</span>
           </li>
         </ul>
       </header>
